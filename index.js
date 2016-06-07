12fe9c861552675b3c0a6b29950211afe3f67625
tran <tran@silpion.de>
2016-06-07 17:31:35 +0200

    update package.json

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+/**
+ * @module
+ * @description
+ * Starting point to import all public core APIs.
+ */
+__export(require('./app/shared/cms/cms'));
+__export(require('./app/shared/route/dynamic-route'));
+__export(require('./app/views/main-page/cms-container'));
+__export(require('./app/views/main-page/cms-element'));
+__export(require('./app/views/main-page/cms-wrapper'));
+__export(require('./app/views/main-page/main-page'));
+__export(require('./app/views/main-page/cms-fragment'));
