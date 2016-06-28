723afda54679665c71f253705fec80478329a6d3
tran <tran@silpion.de>
2016-06-29 00:11:59 +0200

    update nativescript-angular to 1.8

--- app/main.ts
+++ app/main.ts
@@ -3,9 +3,8 @@ import {AppComponent} from "./app.component";
 import {setStatusBarColors} from "./utils/status-bar-util";
 import {Cms} from "./shared/cms/cms";
 import {enableProdMode} from '@angular/core';
-import {DynamicRouteConfigurator} from "./shared/route/dynamic-route";
 import {APP_ROUTER_PROVIDERS} from "./app.route";
 
 // enableProdMode();
 setStatusBarColors();
-nativeScriptBootstrap(AppComponent, [DynamicRouteConfigurator, Cms, APP_ROUTER_PROVIDERS]);
+nativeScriptBootstrap(AppComponent, [Cms, APP_ROUTER_PROVIDERS]);
