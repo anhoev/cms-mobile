397fc3e3fb91b127e4e52dc63951a5261fbf5e2b
tran <tran@silpion.de>
2016-06-29 00:05:30 +0200

    update nativescript-angular to 1.8

---
+++ app/app.route.js
@@ -0,0 +1,7 @@
+"use strict";
+var router_1 = require("nativescript-angular/router");
+exports.routes = [];
+exports.APP_ROUTER_PROVIDERS = [
+    router_1.nsProvideRouter(exports.routes, {})
+];
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLnJvdXRlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiYXBwLnJvdXRlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFDQSx1QkFBOEIsNkJBQTZCLENBQUMsQ0FBQTtBQUUvQyxjQUFNLEdBQWlCLEVBQUUsQ0FBQztBQUUxQiw0QkFBb0IsR0FBRztJQUNoQyx3QkFBZSxDQUFDLGNBQU0sRUFBRSxFQUFFLENBQUM7Q0FDOUIsQ0FBQyJ9
